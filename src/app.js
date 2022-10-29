9d02d54163dc9b42395be09ea5fff9053d21f334
Alexander Alarcon <walarcon94@hotmail.com>
2022-10-29 15:11:51 -0500

    Se agrega prettier y el comando format

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const logger = require('morgan');
-const database = require("./config/database");
+const database = require('./config/database');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
@@ -11,18 +11,14 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-// Mongo connection
 database.mongoConnect();
-
-// router
-
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // error handler
-app.use(function(err, req, res, next) {
- return res.json("errores")
+app.use(function (err, req, res, next) {
+  return res.json('errores');
 });
 
 module.exports = app;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,10 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+
+const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  //res.render('index', { title: 'e-commerce Los Pixies' });
-  res.send("Pruebas")
+router.get('/', function (req, res, next) {
+  res.send('Pruebas');
 });
 
 module.exports = router;
