a96e376eb16eb2f7233177f441c62a4e610bbfb1
AlexandraHD <alexa.her.duarte@gmail.com>
2022-11-19 11:57:09 -0500

    se crea controlador de ordenes

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,7 +1,10 @@
 const productController = require('./product.controller');
+const orderController = require('./order.controller');
 const userController = require('./user.controller');
 
 module.exports = {
   Product: productController,
+  Order: orderController,
   User: userController,
 };
+
