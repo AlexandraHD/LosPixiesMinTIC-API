889d5e408c05bf20c161d56d2feacdd1416dad91
AlexandraHD <alexa.her.duarte@gmail.com>
2022-11-22 20:07:58 -0500

    Se agregan variables y cors

--- src/app.js
+++ src/app.js
@@ -4,9 +4,15 @@ const logger = require('morgan');
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const database = require('./config/database');
+const { CLIENT } = require('./config/variables');
 
 const app = express();
+const corsOptions = {
+  origin: CLIENT,
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
 
+app.use(cors(corsOptions));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
