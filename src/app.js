6fefe27f38be8eee94faab38ed56a7268f870919
Alexander Alarcon <waalarcon2@misena.edu.co>
2022-11-23 10:08:56 -0500

    Se agrega documentacion para /user

--- src/app.js
+++ src/app.js
@@ -1,18 +1,38 @@
+const swaggerUI = require('swagger-ui-express');
+const swaggerJSDoc = require('swagger-jsdoc');
 const express = require('express');
 const logger = require('morgan');
+const cors = require('cors');
 
 const productsRouter = require('./routes/product.router');
 const ordersRouter = require('./routes/order.router');
 const usersRouter = require('./routes/users.router');
 const { Error, NotFound } = require('./middleware');
+const { CLIENT } = require('./config/variables');
 const database = require('./config/database');
 const indexRouter = require('./routes/index');
-const { CLIENT } = require('./config/variables');
+
+/**
+ * @type {swaggerJSDoc.Options}
+ */
+const swaggerOptions = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'E-commerce API',
+      version: '1.0.0',
+    },
+  },
+  apis: ['./src/routes/*.router.js'],
+};
+
+const swaggerSpec = swaggerJSDoc(swaggerOptions);
 
 const app = express();
+
 const corsOptions = {
   origin: CLIENT,
-  optionsSuccessStatus: 200, 
+  optionsSuccessStatus: 200,
 };
 
 app.use(cors(corsOptions));
@@ -26,8 +46,9 @@ app.use('/', indexRouter);
 app.use('/user', usersRouter);
 app.use('/product', productsRouter);
 app.use('/order', ordersRouter);
+app.use('/docs', (_req, res, _next) => res.send(swaggerSpec));
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerSpec));
 
-// error handler
 app.use(NotFound);
 app.use(Error);
 
